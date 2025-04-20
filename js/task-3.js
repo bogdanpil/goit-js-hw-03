0e2bb30db24187bcb671f789a8f95cfa6369cd0e
Bogdan <pilipaka@gmail.com>
2025-04-20 20:32:45 +0300

    done hw-js-3, 3 tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,19 @@
-//Width of the element
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
-    return contentWidth + paddingWidth * 2 + borderWidth * 2;
+// FilterArray
+
+function filterArray(numbers, value) {
+    const filteredNum = []; 
+    
+    for (let i = 0; i < numbers.length; i += 1) { 
+        if (numbers[i] > value) { 
+            filteredNum.push(numbers[i]); 
+        }
+    }
+        return filteredNum;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+
